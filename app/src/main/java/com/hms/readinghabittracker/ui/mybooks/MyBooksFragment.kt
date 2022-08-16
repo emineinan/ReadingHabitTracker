a96274970a1b4ff9289c85c90b023fe97a1ae564
Emine İNAN <56589369+emineinan@users.noreply.github.com>
2022-08-16 16:21:14 +0300

    Add options menu

--- app/src/main/java/com/hms/readinghabittracker/ui/mybooks/MyBooksFragment.kt
+++ app/src/main/java/com/hms/readinghabittracker/ui/mybooks/MyBooksFragment.kt
@@ -1,6 +1,10 @@
 package com.hms.readinghabittracker.ui.mybooks
 
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.fragment.app.viewModels
+import com.hms.readinghabittracker.R
 import com.hms.readinghabittracker.base.BaseFragment
 import com.hms.readinghabittracker.databinding.FragmentMyBooksBinding
 
@@ -8,4 +12,26 @@ class MyBooksFragment :
     BaseFragment<FragmentMyBooksBinding, MyBooksViewModel>(FragmentMyBooksBinding::inflate) {
 
     override val viewModel: MyBooksViewModel by viewModels()
+
+    override fun setupUi() {
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.my_books_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_add -> {
+                // Navigate to Add Book Fragment
+                true
+            }
+            R.id.action_collections -> {
+                // Navigate to Collections Fragment
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
