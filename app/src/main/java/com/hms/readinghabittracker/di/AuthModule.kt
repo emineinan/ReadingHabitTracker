fecf17399795164bc3a94ec65b9d86ebb0e4fb53
Emine İNAN <56589369+emineinan@users.noreply.github.com>
2022-08-30 21:50:08 +0300

    Move auth operations to AuthModule

--- app/src/main/java/com/hms/readinghabittracker/di/AuthModule.kt
+++ app/src/main/java/com/hms/readinghabittracker/di/AuthModule.kt
@@ -1,6 +1,10 @@
 package com.hms.readinghabittracker.di
 
 import android.content.Context
+import com.huawei.agconnect.AGCRoutePolicy
+import com.huawei.agconnect.AGConnectInstance
+import com.huawei.agconnect.AGConnectOptionsBuilder
+import com.huawei.agconnect.auth.AGConnectAuth
 import com.huawei.hms.support.hwid.HuaweiIdAuthManager
 import com.huawei.hms.support.hwid.request.HuaweiIdAuthParams
 import com.huawei.hms.support.hwid.request.HuaweiIdAuthParamsHelper
@@ -30,5 +34,24 @@ class AuthModule {
         huaweiIdAuthParams: HuaweiIdAuthParams
     ): HuaweiIdAuthService {
         return HuaweiIdAuthManager.getService(context, huaweiIdAuthParams)
+    }
+
+    @Singleton
+    @Provides
+    fun provideAGConnectInstance(
+        @ApplicationContext context: Context
+    ): AGConnectInstance {
+        val agcConnectOptions =
+            AGConnectOptionsBuilder().setRoutePolicy(AGCRoutePolicy.GERMANY).build(context)
+        return AGConnectInstance.buildInstance(agcConnectOptions)
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideAGConnectAuth(
+        agConnectInstance: AGConnectInstance
+    ): AGConnectAuth {
+        return AGConnectAuth.getInstance(agConnectInstance)
     }
 }
--- app/src/main/java/com/hms/readinghabittracker/di/CloudDbModule.kt
+++ app/src/main/java/com/hms/readinghabittracker/di/CloudDbModule.kt
@@ -1,15 +1,11 @@
 package com.hms.readinghabittracker.di
 
-import android.content.Context
-import com.huawei.agconnect.AGCRoutePolicy
 import com.huawei.agconnect.AGConnectInstance
-import com.huawei.agconnect.AGConnectOptionsBuilder
 import com.huawei.agconnect.auth.AGConnectAuth
 import com.huawei.agconnect.cloud.database.AGConnectCloudDB
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -27,24 +23,5 @@ object CloudDBModule {
             agConnectInstance,
             agConnectAuth
         )
-    }
-
-    @Singleton
-    @Provides
-    fun provideAGConnectInstance(
-        @ApplicationContext context: Context
-    ): AGConnectInstance {
-        val agcConnectOptions =
-            AGConnectOptionsBuilder().setRoutePolicy(AGCRoutePolicy.GERMANY).build(context)
-        return AGConnectInstance.buildInstance(agcConnectOptions)
-    }
-
-
-    @Singleton
-    @Provides
-    fun provideAGConnectAuth(
-        agConnectInstance: AGConnectInstance
-    ): AGConnectAuth {
-        return AGConnectAuth.getInstance(agConnectInstance)
     }
 }
