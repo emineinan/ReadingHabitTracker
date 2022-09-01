652a5c5eb3a767dbfd78da16079c581485979453
Emine İNAN <56589369+emineinan@users.noreply.github.com>
2022-09-01 14:12:01 +0300

    Add collection dialog

--- app/src/main/java/com/hms/readinghabittracker/ui/collections/CollectionsFragment.kt
+++ app/src/main/java/com/hms/readinghabittracker/ui/collections/CollectionsFragment.kt
@@ -1,18 +1,39 @@
 package com.hms.readinghabittracker.ui.collections
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.app.AlertDialog
 import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
 import com.hms.readinghabittracker.R
+import com.hms.readinghabittracker.base.BaseFragment
+import com.hms.readinghabittracker.databinding.AddCollectionDialogBinding
+import com.hms.readinghabittracker.databinding.FragmentCollectionsBinding
 
-class CollectionsFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_collections, container, false)
+class CollectionsFragment :
+    BaseFragment<FragmentCollectionsBinding, CollectionsViewModel>(FragmentCollectionsBinding::inflate) {
+    override val viewModel: CollectionsViewModel by viewModels()
+
+    override fun setupUi() {
+        binding.fabAddCollection.setOnClickListener {
+            showAddCollectionDialog()
+        }
+    }
+
+    private fun showAddCollectionDialog() {
+        val dialogBinding: AddCollectionDialogBinding =
+            AddCollectionDialogBinding.inflate(LayoutInflater.from(context))
+        val builder = AlertDialog.Builder(context).setView(dialogBinding.root).show()
+        val addCollection = dialogBinding.buttonAddCollection
+        val collectionName = dialogBinding.editTextCollectionName
+
+        addCollection.setOnClickListener {
+            val title = collectionName.text.toString()
+            if (title.isEmpty()) {
+                Toast.makeText(context, R.string.empty_field_warning, Toast.LENGTH_SHORT).show()
+            } else {
+                // Add collection to CloudDb
+                builder.dismiss()
+            }
+        }
     }
 }
