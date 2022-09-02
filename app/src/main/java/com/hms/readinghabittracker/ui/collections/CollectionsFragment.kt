eeab1b4fc324a4a65b2932bd19492fa10c374706
Emine İNAN <56589369+emineinan@users.noreply.github.com>
2022-09-02 10:19:58 +0300

    Add collection to CloudDb

--- app/src/main/java/com/hms/readinghabittracker/ui/collections/CollectionsFragment.kt
+++ app/src/main/java/com/hms/readinghabittracker/ui/collections/CollectionsFragment.kt
@@ -4,14 +4,25 @@ import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.hms.readinghabittracker.R
 import com.hms.readinghabittracker.base.BaseFragment
 import com.hms.readinghabittracker.databinding.AddCollectionDialogBinding
 import com.hms.readinghabittracker.databinding.FragmentCollectionsBinding
+import com.huawei.agconnect.auth.AGConnectAuth
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class CollectionsFragment :
     BaseFragment<FragmentCollectionsBinding, CollectionsViewModel>(FragmentCollectionsBinding::inflate) {
     override val viewModel: CollectionsViewModel by viewModels()
+
+    @Inject
+    lateinit var agConnect: AGConnectAuth
 
     override fun setupUi() {
         binding.fabAddCollection.setOnClickListener {
@@ -27,12 +38,35 @@ class CollectionsFragment :
         val collectionName = dialogBinding.editTextCollectionName
 
         addCollection.setOnClickListener {
-            val title = collectionName.text.toString()
-            if (title.isEmpty()) {
+            val collectionNameText = collectionName.text.toString()
+            if (collectionNameText.isEmpty()) {
                 Toast.makeText(context, R.string.empty_field_warning, Toast.LENGTH_SHORT).show()
             } else {
-                // Add collection to CloudDb
+                viewModel.saveCollectionToCloudDb(agConnect, collectionNameText)
                 builder.dismiss()
+            }
+        }
+    }
+
+    override fun setupObserver() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.collectionsUiState.collect { collectionsUiState ->
+                    collectionsUiState.error.firstOrNull()?.let {
+                        Toast.makeText(
+                            requireContext(),
+                            "Collection is not saved, there is an error...",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    collectionsUiState.isCollectionSaved.firstOrNull()?.let {
+                        Toast.makeText(
+                            requireContext(),
+                            "Collection is saved successfully.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
             }
         }
     }
