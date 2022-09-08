a0dd3fdbff1a7bb26e3bc04ddb4f8cf134802a95
Emine İNAN <56589369+emineinan@users.noreply.github.com>
2022-09-08 14:40:46 +0300

    Update MyBooksAdapter.kt

--- app/src/main/java/com/hms/readinghabittracker/ui/mybooks/adapter/MyBooksAdapter.kt
+++ app/src/main/java/com/hms/readinghabittracker/ui/mybooks/adapter/MyBooksAdapter.kt
@@ -1,15 +1,18 @@
 package com.hms.readinghabittracker.ui.mybooks.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.hms.readinghabittracker.R
 import com.hms.readinghabittracker.data.model.Collection
+import com.hms.readinghabittracker.data.model.CollectionUIModel
 import com.hms.readinghabittracker.databinding.MyBooksItemBinding
 
-class MyBooksAdapter(private val collections: List<Collection>) :  //Parent Adapter
+class MyBooksAdapter() :  //Parent Adapter
     RecyclerView.Adapter<MyBooksAdapter.MyBooksViewHolder>() {
+    private var collections = listOf<CollectionUIModel>()
 
     class MyBooksViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val binding = MyBooksItemBinding.bind(view)
@@ -25,10 +28,16 @@ class MyBooksAdapter(private val collections: List<Collection>) :  //Parent Adap
         holder.binding.apply {
             val collection = collections[position]
             textViewCollectionName.text = collection.name
-            //val booksAdapter = BookAdapter(we need to our collection's book list, how do we reach this book list?)
-            //recyclerViewBooks.adapter = booksAdapter
+            val booksAdapter = BookAdapter(collection.books)
+            recyclerViewBooks.adapter = booksAdapter
         }
     }
 
     override fun getItemCount() = collections.size
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setCollectionList(collectionList: List<CollectionUIModel>) {
+        this.collections = collectionList
+        notifyDataSetChanged()
+    }
 }
