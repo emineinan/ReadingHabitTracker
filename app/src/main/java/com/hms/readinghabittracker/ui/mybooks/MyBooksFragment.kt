dc27c7b77c833504379fa5a1b13356e09036952b
Emine İNAN <56589369+emineinan@users.noreply.github.com>
2022-09-08 14:41:34 +0300

    Update MyBooksFragment.kt

--- app/src/main/java/com/hms/readinghabittracker/ui/mybooks/MyBooksFragment.kt
+++ app/src/main/java/com/hms/readinghabittracker/ui/mybooks/MyBooksFragment.kt
@@ -1,23 +1,43 @@
 package com.hms.readinghabittracker.ui.mybooks
 
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hms.readinghabittracker.R
 import com.hms.readinghabittracker.base.BaseFragment
+import com.hms.readinghabittracker.data.model.Collection
+import com.hms.readinghabittracker.data.model.CollectionUIModel
+import com.hms.readinghabittracker.data.repository.CloudDbRepository
 import com.hms.readinghabittracker.databinding.FragmentMyBooksBinding
+import com.hms.readinghabittracker.ui.collections.CollectionsAdapter
 import com.hms.readinghabittracker.ui.mybooks.adapter.MyBooksAdapter
+import com.huawei.agconnect.auth.AGConnectAuth
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MyBooksFragment :
+@AndroidEntryPoint
+class MyBooksFragment() :
     BaseFragment<FragmentMyBooksBinding, MyBooksViewModel>(FragmentMyBooksBinding::inflate) {
 
     override val viewModel: MyBooksViewModel by viewModels()
 
+    @Inject
+    lateinit var agConnect: AGConnectAuth
+
+    private var myBooksAdapter = MyBooksAdapter()
+
     override fun setupUi() {
         setHasOptionsMenu(true)
-        setRecyclerView()
+        setAdapter()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -38,7 +58,33 @@ class MyBooksFragment :
         }
     }
 
-    private fun setRecyclerView() {
-        //binding.recyclerViewMyBooks.adapter = MyBooksAdapter(we need to our collections list)
+    override fun setupObserver() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.myBooksUiState.collect { myBooksUiState ->
+                    myBooksUiState.error.firstOrNull()?.let {
+                        Toast.makeText(
+                            requireContext(),
+                            "My books is not display, there is an error...",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    if (myBooksUiState.savedMyBookList.isNotEmpty()) {
+                        myBooksUiState.savedMyBookList.let {
+                            //myBooksAdapter.setCollectionList(it)  // it want collectionUiModel list
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun setAdapter() {
+        binding.recyclerViewMyBooks.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerViewMyBooks.setHasFixedSize(true)
+        binding.recyclerViewMyBooks.adapter = myBooksAdapter
+        //myBooksAdapter.setCollectionList(cloudDbRepository.getCollectionsForCurrentUser(agConnect.currentUser.uid.toLong()))
     }
 }
