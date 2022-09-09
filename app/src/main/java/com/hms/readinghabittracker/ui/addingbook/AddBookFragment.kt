0b884a5d4035387842b63186f857c740aeb2b68e
Emine İNAN <56589369+emineinan@users.noreply.github.com>
2022-09-09 16:08:15 +0300

    Add ImageDialog

--- app/src/main/java/com/hms/readinghabittracker/ui/addingbook/AddBookFragment.kt
+++ app/src/main/java/com/hms/readinghabittracker/ui/addingbook/AddBookFragment.kt
@@ -1,8 +1,12 @@
 package com.hms.readinghabittracker.ui.addingbook
 
+import android.app.AlertDialog
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.hms.readinghabittracker.base.BaseFragment
+import com.hms.readinghabittracker.databinding.AddImageDialogBinding
 import com.hms.readinghabittracker.databinding.FragmentAddBookBinding
 
 class AddBookFragment :
@@ -13,5 +17,27 @@ class AddBookFragment :
 
     override fun setupUi() {
         binding.toolbarAddBook.title = args.collectionName
+
+        binding.imageViewAddIcon.setOnClickListener {
+            showAddBookImageDialog()
+        }
+    }
+
+    private fun showAddBookImageDialog() {
+        val dialogBinding: AddImageDialogBinding =
+            AddImageDialogBinding.inflate(LayoutInflater.from(context))
+        val builder = AlertDialog.Builder(context).setView(dialogBinding.root).show()
+        val cameraSelected = dialogBinding.textViewCamera
+        val gallerySelected = dialogBinding.textViewGallery
+
+        cameraSelected.setOnClickListener {
+            Toast.makeText(requireContext(), "Camera selected.", Toast.LENGTH_SHORT).show()
+            builder.dismiss()
+        }
+
+        gallerySelected.setOnClickListener {
+            Toast.makeText(requireContext(), "Gallery selected.", Toast.LENGTH_SHORT).show()
+            builder.dismiss()
+        }
     }
 }
