c5a5ffb8b1bfb25524cf30379d9a51d4ca598bac
Emine İNAN <56589369+emineinan@users.noreply.github.com>
2022-09-11 14:39:04 +0300

    Create Constants.kt

---
+++ app/src/main/java/com/hms/readinghabittracker/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.hms.readinghabittracker.utils
+
+object Constants {
+    const val PERMISSION_REQUEST_CODE_CAMERA = 1
+    const val CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1045
+    const val REQUEST_PICK_IMAGE = 1001
+}
