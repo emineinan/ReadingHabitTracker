d94d829040017c7425dd512f6e30703efe5ad3e4
Emine İNAN <56589369+emineinan@users.noreply.github.com>
2022-09-13 16:32:24 +0300

    Update Goals Fragment

--- app/src/main/java/com/hms/readinghabittracker/ui/goals/GoalsFragment.kt
+++ app/src/main/java/com/hms/readinghabittracker/ui/goals/GoalsFragment.kt
@@ -2,99 +2,71 @@ package com.hms.readinghabittracker.ui.goals
 
 import android.annotation.SuppressLint
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.MutableLiveData
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.hms.readinghabittracker.R
 import com.hms.readinghabittracker.base.BaseFragment
+import com.hms.readinghabittracker.data.model.GoalItem
 import com.hms.readinghabittracker.databinding.FragmentGoalsBinding
 import com.hms.readinghabittracker.utils.PermissionUtils
 import com.hms.readinghabittracker.utils.TimeUtils
-import com.hms.readinghabittracker.utils.dialog.DaysDialog
-import com.hms.readinghabittracker.utils.dialog.TimeDialog
 import com.huawei.hms.kit.awareness.Awareness
 import com.huawei.hms.kit.awareness.capture.TimeCategoriesResponse
+import dagger.hilt.android.AndroidEntryPoint
 import pub.devrel.easypermissions.AppSettingsDialog
 import pub.devrel.easypermissions.EasyPermissions
 
+@AndroidEntryPoint
 class GoalsFragment :
     BaseFragment<FragmentGoalsBinding, GoalsViewModel>(FragmentGoalsBinding::inflate),
     EasyPermissions.PermissionCallbacks {
 
     override val viewModel: GoalsViewModel by viewModels()
+    private lateinit var recyclerView: RecyclerView
+    private val showDone: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>(false)
+    }
 
-    val selectedDays: MutableList<Int> = ArrayList()
-    var selectedHour: Int? = null
-    var selectedMinute: Int? = null
 
     override fun setupUi() {
         requestPermissions()
 
-        binding.buttonDays.setOnClickListener {
-            daysDialogCreate()
+        binding.floatingActionButtonGoal.setOnClickListener {
+            findNavController().navigate(R.id.action_goalsFragment_to_addGoalFragment)
         }
-        binding.buttonTime.setOnClickListener {
-            timeDialogCreate()
-        }
-        binding.buttonSaveGoals.setOnClickListener {
 
-        }
-    }
+        recyclerView = binding.recyclerViewGoals
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        val adapter = GoalsAdapter()
+        recyclerView.adapter = adapter
 
-    private fun daysDialogCreate() {
-        val customDialog = DaysDialog.getInstance(requireContext())
-            .setPositiveButton(
-                "Ok",
-                object : DaysDialog.ICustomDialogClickListener {
-                    override fun onClick() {
-                    }
-                }) {
-                binding.buttonDays.text = it.toString()
-                selectedDays.clear()
-                for (i in it) {
-                    when (i) {
-                        "Sunday" -> {
-                            selectedDays.add(1)
-                        }
-                        "Monday" -> {
-                            selectedDays.add(2)
-                        }
-                        "Tuesday" -> {
-                            selectedDays.add(3)
-                        }
-                        "Wednesday" -> {
-                            selectedDays.add(4)
-                        }
-                        "Thursday" -> {
-                            selectedDays.add(5)
-                        }
-                        "Friday" -> {
-                            selectedDays.add(6)
-                        }
-                        "Saturday" -> {
-                            selectedDays.add(7)
-                        }
-                    }
+        var observableList = MutableLiveData<List<GoalItem>>()
+
+        showDone.observe(this.viewLifecycleOwner) { newValue ->
+            observableList.removeObservers(this.viewLifecycleOwner)
+
+            if (newValue == true) {
+                observableList = viewModel.allGoalItemsDone as MutableLiveData
+                if (activity is AppCompatActivity) {
+                    (activity as AppCompatActivity).supportActionBar?.title = "Goal done"
+                }
+            } else {
+                observableList = viewModel.allGoalItems as MutableLiveData
+                if (activity is AppCompatActivity) {
+                    (activity as AppCompatActivity).supportActionBar?.title = "Goal to do"
                 }
             }
-            .setCancelButton("Cancel")
-            .createDialog()
-        customDialog.showDialog()
-    }
-
-    private fun timeDialogCreate() {
-        val customDialog = TimeDialog.getInstance(requireContext())
-            .setPositiveButton(
-                "Ok",
-                object : TimeDialog.ICustomDialogClickListener {
-                    override fun onClick() {
-                    }
-                }) {
-                binding.buttonTime.text = it[0].toString() + ":" + it[1].toString()
-                selectedHour = it[0]
-                selectedMinute = it[1]
+            observableList.observe(this.viewLifecycleOwner) { items ->
+                items.let {
+                    adapter.submitList(it)
+                }
             }
-            .setCancelButton("Cancel")
-            .createDialog()
-        customDialog.showDialog()
+        }
+
     }
 
     private fun requestPermissions() {
