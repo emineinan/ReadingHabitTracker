261ece41b1cb60567b49a26dc5c460666837ce7c
Emine İNAN <56589369+emineinan@users.noreply.github.com>
2022-09-15 14:27:11 +0300

    Update Library.kt

--- app/src/main/java/com/hms/readinghabittracker/data/model/Library.kt
+++ app/src/main/java/com/hms/readinghabittracker/data/model/Library.kt
@@ -1,9 +1,12 @@
 package com.hms.readinghabittracker.data.model
 
+import android.os.Parcelable
 import com.huawei.hms.site.api.model.Coordinate
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Library(
     val name: String,
     val address: String,
     val location: Coordinate
-)
+):Parcelable
