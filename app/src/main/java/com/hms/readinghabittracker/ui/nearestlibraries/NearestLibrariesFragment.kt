69d9eec9ed35977a09775c0139a84af3f27c6745
Emine İNAN <56589369+emineinan@users.noreply.github.com>
2022-09-15 14:28:56 +0300

    Update Nearest Libraries Fragment

--- app/src/main/java/com/hms/readinghabittracker/ui/nearestlibraries/NearestLibrariesFragment.kt
+++ app/src/main/java/com/hms/readinghabittracker/ui/nearestlibraries/NearestLibrariesFragment.kt
@@ -2,13 +2,16 @@ package com.hms.readinghabittracker.ui.nearestlibraries
 
 import android.os.Looper
 import android.util.Log
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.hms.readinghabittracker.R
 import com.hms.readinghabittracker.base.BaseFragment
 import com.hms.readinghabittracker.data.model.Library
 import com.hms.readinghabittracker.databinding.FragmentNearestLibrariesBinding
+import com.hms.readinghabittracker.utils.Constant.MAP_KEY
 import com.hms.readinghabittracker.utils.PermissionUtils
 import com.huawei.hms.location.*
 import com.huawei.hms.site.api.SearchResultListener
@@ -31,7 +34,6 @@ class NearestLibrariesFragment :
     private var currentLatitude = 0.0
     private var currentLongitude = 0.0
     private var libraryList = arrayListOf<Library>()
-    private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: NearestLibrariesAdapter
 
     override fun setupUi() {
@@ -71,7 +73,7 @@ class NearestLibrariesFragment :
         val searchService = SearchServiceFactory.create(
             requireContext(),
             URLEncoder.encode(
-                "DAEDAAckTAdZ/aWT5I35ayZZQXYPLugtG7hgfF3wAVnK6s9XutrbTnts4KhSi3NVxIyofRq5EvzlCUR0AFGoJRoa6LtFEOB5TuJ2Vg==",
+                MAP_KEY,
                 "utf-8"
             )
         )
@@ -93,17 +95,21 @@ class NearestLibrariesFragment :
                         )
                         libraryList.add(library)
                     }
-                    setAdapter()
+                    if (it.sites.isNotEmpty()) {
+                        setAdapter()
+                        binding.progressBarNearestLibraries.visibility = View.INVISIBLE
+                    } else {
+                        binding.progressBarNearestLibraries.visibility = View.VISIBLE
+                    }
                 }
             }
         })
     }
 
     private fun setAdapter() {
-        recyclerView = binding.recyclerViewNearestLibraries
-        recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerViewNearestLibraries.layoutManager = LinearLayoutManager(context)
         adapter = NearestLibrariesAdapter(libraryList)
-        recyclerView.adapter = adapter
+        binding.recyclerViewNearestLibraries.adapter = adapter
     }
 
     private fun requestPermissions() {
